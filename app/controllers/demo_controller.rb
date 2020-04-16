99fade1b0170b347577729ae93ca72017633778c
Bryan Siegel <bryansiegel@gmail.com>
2020-04-16 09:22:53 -0700

    subjects

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -4,6 +4,26 @@ class DemoController < ApplicationController
 
   def hello
     @array = [1,2,3,4,5]
+    @id = params['id']
+    @page = params[:page]
   end
+
+  def about
+    render "demo/about_us"  
+    
+  end
+
+  def contact
+    if ['us', 'ca'].include?(params[:country])
+      @phone = "(800) 123 - 2323"
+    elsif params['country'] == "uk"
+      @phone = "+1 4544 - 2343"
+    else
+      @phone = "123123123123"
+    end
+    render "demo/contact_us"
+  end
+  
+  
 
 end
