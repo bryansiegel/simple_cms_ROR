5cb1ba5a69546bf7f760968d24c86601c9160e0a
Bryan Siegel <bryansiegel@gmail.com>
2020-04-16 10:33:12 -0700

    edit update

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -14,12 +14,26 @@ class SubjectsController < ApplicationController
   end
 
   def create
+    @subject = Subject.new(subject_params)
+    if @subject.save
+      redirect_to subjects_path
+    else
+      render 'new'
+    end
+
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+     @subject = Subject.find(params[:id])
+      if @subject.update(subject_params)
+        redirect_to subject_path(@subject)
+      else
+        render 'edit'
+      end
   end
 
   def delete
@@ -27,5 +41,12 @@ class SubjectsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def subject_params
+        params.require(:subject).permit(:name, :visible, :position)
+  end
+  
   
 end
